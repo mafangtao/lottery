a4558de07a4d78dc3417d27285895bc7460464ef
tom <tomas0447@outlook.com>
2021-05-16 10:20:51 +0800

    add frontend

--- truffle-config.js
+++ truffle-config.js
@@ -102,8 +102,8 @@ module.exports = {
 
   // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
   //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // Note: if you migrated your contract prior to enabling this field in your Truffle project and want
+  // those previously migrated contract available in the .db directory, you will need to run the following:
   // $ truffle migrate --reset --compile-all
 
   db: {
